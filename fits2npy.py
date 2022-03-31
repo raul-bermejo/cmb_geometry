cc80ffdbd0381d65da9abc012b071bc10d894cc4
Raul <raulbv.personal@gmail.com>
2022-03-31 12:43:08 +1100

    Updated fits2py for testing

--- fits2npy.py
+++ fits2npy.py
@@ -42,8 +42,8 @@ def extract_data(filepath, hdu=1):
     
     return data
 
-def fits2npy(filename, field,
-             beamfunc=False):
+def fits2npy(filename, 
+            field, hdu=1, beamfunc=False):
     '''
     Extracts useful data from FITS file and saves it as .npy file
     Document function once fully tested
@@ -54,18 +54,18 @@ def fits2npy(filename, field,
 
     short_name = filename.split('.fits')[0]
 
-
-    array = read_map('data/', field=field, hdu=HDU)
+    if beamfunc:
+        array = extract_data(filename)
+    else:
+        array = read_map('data/'+filename, field=field, hdu=HDU)
 
     array = np.array(array.astype(np.float))
-    np.save('data/'+short_name+'.npy', array)
+    np.save('data_test/'+short_name+'.npy', array)
 
     print(f"Data was extracted and saved into {'data/'+short_name+'.npy'} succesfully")
     print('-'*100)
 
     return None
-
-
 
 # define filenames as downloaded from planck release: https://pla.esac.esa.int/#home
 filenames = ['HFI_SkyMap_143_2048_R3.01_halfmission-1.fits',        # map for half mission 1 143 GHZ
@@ -78,27 +78,27 @@ filenames = ['HFI_SkyMap_143_2048_R3.01_halfmission-1.fits',        # map for ha
 
 start = time.time()
 # define filepaths
-filepath_hm1 = 'data/HFI_SkyMap_143_2048_R3.01_halfmission-1.fits'         # map for half mission 1 143 GHZ
-filepath_hm2 = 'data/HFI_SkyMap_143_2048_R3.01_halfmission-2.fits'         # map for half mission 2 143 GHZ
-filepath_gp = f'data/HFI_Mask_GalPlane-apo{APO}_2048_R2.00.fits'           # galactic plane mask for input apodization length
-filepath_ps = 'data/HFI_Mask_PointSrc_2048_R2.00.fits'                     # mask point source
-filepath_beamwin1 = 'data/Bl_T_R3.01_fullsky_143hm1x143hm1.fits'           # beam transfer function for hm1
-filepath_beamwin2 = 'data/Bl_T_R3.01_fullsky_143hm1x143hm2.fits'           # beam transfer function for hm2
+filepath_hm1 = 'HFI_SkyMap_143_2048_R3.01_halfmission-1.fits'         # map for half mission 1 143 GHZ
+filepath_hm2 = 'HFI_SkyMap_143_2048_R3.01_halfmission-2.fits'         # map for half mission 2 143 GHZ
+filepath_gp = f'HFI_Mask_GalPlane-apo{APO}_2048_R2.00.fits'           # galactic plane mask for input apodization length
+filepath_ps = 'HFI_Mask_PointSrc_2048_R2.00.fits'                     # mask point source
+filepath_beamwin1 = 'Bl_T_R3.01_fullsky_143hm1x143hm1.fits'           # beam transfer function for hm1
+filepath_beamwin2 = 'Bl_T_R3.01_fullsky_143hm1x143hm2.fits'           # beam transfer function for hm2
          
 start1 = time.time()
 print(f'Started Planck data extraction:')
 print('='*80)
 # extract data by using read_map funcitn from healpy
-sky_hm1, sky_hm2 = read_map(filepath_hm1, field=0, hdu=HDU), read_map(filepath_hm2, field=0, hdu=HDU)
+sky_hm1, sky_hm2 = fits2npy(filepath_hm1, field=0, hdu=HDU), fits2npy(filepath_hm2, field=0, hdu=HDU)
 
 field_gp = GALCOVER_DICT[f'GAL0{GAL_COVER}']
-mask_gp = read_map(filepath_gp, field=field_gp, hdu=HDU)
+mask_gp = fits2npy(filepath_gp, field=field_gp, hdu=HDU)
 
 field_ps = FREQ_PS_DICT['F143']
-mask_ps = read_map(filepath_ps, field=field_ps, hdu=HDU)
+mask_ps = fits2npy(filepath_ps, field=field_ps, hdu=HDU)
 
 # use the extract_data function to find the beam window function
-beam_hm1, beam_hm2 = extract_data(filepath_beamwin1), extract_data(filepath_beamwin2)
+beam_hm1, beam_hm2 = fits2npy(filepath_beamwin1, beamfunc=True), fits2npy(filepath_beamwin2, beamfunc=True)
 
 end = time.time()
 print(f'='*130)
